519aa86a04eebcb7f01acec81a5a1ee70541993d
omarsuyuf <omarsuyufw@gmail.com>
2025-09-09 23:02:19 +0700

    update: isi README dan perbaikan tugas 2

--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Product
+
 
 def home(request):
-    ctx = {"app_name": "Football Shop", "name": "Nama Kamu", "class": "Kelas Kamu"}
+    ctx = {"app_name": "Football Shop", 
+           "name": "Omar Suyuf Wicaksono", 
+           "class": "PBP E",
+           "npm" : "2406421200",
+           "products": Product.objects.all()}
     return render(request, "main.html", ctx)
